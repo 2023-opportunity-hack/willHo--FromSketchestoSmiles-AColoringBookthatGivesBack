e70df674df85b6b7ffd2d44938b522e69134f2ce
anirudhpra5062 <67618134+anirudhpra5062@users.noreply.github.com>
2023-10-08 00:52:54 -0500

    Add files via upload

---
+++ Start Fundraiser.js
@@ -0,0 +1,3 @@
+export default function StartFundraiser(){
+    return <h1>Start Fundraiser!</h1>
+}
---
+++ UploadPictures.js
@@ -0,0 +1,3 @@
+export default function UploadPictures(){
+    return <h1>Upload your Pictures Here! This will generate an AI Coloring Book!!!</h1>
+}
